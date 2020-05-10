f26898c9818bba8f497d7d34cdf24e7af92d64d1
NAGARJUNA REDDY <NAGARJUNA REDDY@LAPTOP-U6TEQBSS>
2020-05-10 22:08:07 +0530

    no

--- src/JunitPackage/AllTests.java
+++ src/JunitPackage/AllTests.java
@@ -6,6 +6,6 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ testAddNumbers.class, testAddStrings.class })
-public class AllTests {
+public class  AllTests {
 
 }
